6d2c71629f985ebcbe0026ba7b80d8038978c0dc
Tyler <tyler.stirtz@gmail.com>
2021-12-23 20:49:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,162 @@
+(function () {
+
+    var COUNT = 1200;
+    var masthead = document.querySelector('.sky');
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var width = masthead.clientWidth;
+    var height = masthead.clientHeight;
+    var i = 0;
+    var active = false;
+  
+    function onResize() {
+      width = masthead.clientWidth;
+      height = masthead.clientHeight;
+      canvas.width = width;
+      canvas.height = height;
+      ctx.fillStyle = '#FFF';
+  
+      var wasActive = active;
+      active = width > 300;
+  
+      if (!wasActive && active)
+        requestAnimFrame(update);
+    }
+  
+    var Snowflake = function () {
+      this.x = 0;
+      this.y = 0;
+      this.vy = 0;
+      this.vx = 0;
+      this.r = 0;
+  
+      this.reset();
+    }
+  
+    Snowflake.prototype.reset = function() {
+      this.x = Math.random() * width;
+      this.y = Math.random() * -height;
+      this.vy = 1 + Math.random() * 3;
+      this.vx = 0.5 - Math.random();
+      this.r = 1 + Math.random() * 2;
+      this.o = 0.5 + Math.random() * 0.5;
+    }
+  
+    canvas.style.position = 'absolute';
+    canvas.style.left = canvas.style.top = '0';
+  
+    var snowflakes = [], snowflake;
+    for (i = 0; i < COUNT; i++) {
+      snowflake = new Snowflake();
+      snowflake.reset();
+      snowflakes.push(snowflake);
+    }
+  
+    function update() {
+  
+      ctx.clearRect(0, 0, width, height);
+  
+      if (!active)
+        return;
+  
+      for (i = 0; i < COUNT; i++) {
+        snowflake = snowflakes[i];
+        snowflake.y += snowflake.vy;
+        snowflake.x += snowflake.vx;
+  
+        ctx.globalAlpha = snowflake.o;
+        ctx.beginPath();
+        ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+  
+        if (snowflake.y > height) {
+          snowflake.reset();
+        }
+      }
+  
+      requestAnimFrame(update);
+    }
+  
+    // shim layer with setTimeout fallback
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+  
+    onResize();
+    window.addEventListener('resize', onResize, false);
+  
+    masthead.appendChild(canvas);
+  })();
+
+  // Higher number = more zoom
+let scaleAmount = 0.65;
+
+function scrollZoom() {
+  const images = document.querySelectorAll("[data-scroll-zoom]");
+  let scrollPosY = 0;
+  scaleAmount = scaleAmount / 100;
+
+  const observerConfig = {
+    rootMargin: "0% 0% 0% 0%",
+    threshold: 0
+  };
+
+  // Create separate IntersectionObservers and scroll event listeners for each image so that we can individually apply the scale only if the image is visible
+  images.forEach(image => {
+    let isVisible = false;
+    const observer = new IntersectionObserver((elements, self) => {
+      elements.forEach(element => {
+        isVisible = element.isIntersecting;
+      });
+    }, observerConfig);
+
+    observer.observe(image);
+
+    // Set initial image scale on page load
+    image.style.transform = `scale(${1 + scaleAmount * percentageSeen(image)})`;
+
+    // Only fires if IntersectionObserver is intersecting
+    window.addEventListener("scroll", () => {
+      if (isVisible) {
+        scrollPosY = window.pageYOffset;
+        image.style.transform = `scale(${1 +
+          scaleAmount * percentageSeen(image)})`;
+      }
+    });
+  });
+
+  // Calculates the "percentage seen" based on when the image first enters the screen until the moment it leaves
+  // Here, we get the parent node position/height instead of the image since it's in a container that has a border, but
+  // if your container has no extra height, you can simply get the image position/height
+  function percentageSeen(element) {
+    const parent = element.parentNode;
+    const viewportHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+    const elPosY = parent.getBoundingClientRect().top + scrollY;
+    const borderHeight = parseFloat(getComputedStyle(parent).getPropertyValue('border-bottom-width')) + parseFloat(getComputedStyle(element).getPropertyValue('border-top-width'));
+    const elHeight = parent.offsetHeight + borderHeight;
+
+    if (elPosY > scrollY + viewportHeight) {
+      // If we haven't reached the image yet
+      return 0;
+    } else if (elPosY + elHeight < scrollY) {
+      // If we've completely scrolled past the image
+      return 100;
+    } else {
+      // When the image is in the viewport
+      const distance = scrollY + viewportHeight - elPosY;
+      let percentage = distance / ((viewportHeight + elHeight) / 100);
+      percentage = Math.round(percentage);
+
+      return percentage;
+    }
+  }
+}
+
+scrollZoom();
